ba9e2c003c7720feb905ab45e89dd16e9b5af377
AnanyaNunDesai <ananyadesai290402@gmail.com>
2023-09-11 21:09:22 +0530

    changed server code

---
+++ todo-backend/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(bodyParser.json());
+
+// In-memory storage for tasks
+let tasks = [];
+
+// RESTful API endpoints
+
+// Get all tasks
+app.get("/api/tasks", (req, res) => {
+  res.json(tasks);
+});
+
+// Add a new task
+app.post("/api/tasks", (req, res) => {
+  const { text } = req.body;
+  if (text) {
+    const newTask = { id: Date.now(), text };
+    tasks.push(newTask);
+    res.status(201).json(newTask);
+  } else {
+    res.status(400).json({ error: "Task text is required." });
+  }
+});
+
+// Delete a task by ID
+app.delete("/api/tasks/:id", (req, res) => {
+  const taskId = parseInt(req.params.id);
+  tasks = tasks.filter((task) => task.id !== taskId);
+  res.status(204).send();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
